f6e2e87b4ec71e490c9ae6020de29389b0fc89ef
Hildegar Medina <medinahildegar1@gmail.com>
2024-11-18 16:43:26 -0300

    Templates structure and header

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('pages/home.html')
