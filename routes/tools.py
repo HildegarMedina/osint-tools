716aece45784a4e1b57cb6b0d938e7086c781493
Hildegar Medina <medinahildegar1@gmail.com>
2024-11-20 15:49:05 -0300

    Add lookup ip geolocation

--- routes/tools.py
+++ routes/tools.py
@@ -1,10 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from services.ip_geolocation import IpGeolocation
 
 route = Blueprint('tools', __name__)
+ip_geolocation_svc = IpGeolocation()
 
-@route.route('/ip-geolocation')
+@route.route('/ip-geolocation', methods=['GET'])
 def ip_geolocation():
-    return render_template('pages/tools/ip_geolocation.html')
+    return render_template('pages/tools/ip_geolocation/index.html')
+
+@route.route('/ip-geolocation', methods=['POST'])
+def lookup_ip_geolocation():
+    ip_address = request.form['ip_address']
+    details = ip_geolocation_svc.get_details(ip_address)
+    map = ip_geolocation_svc.draw_map(details["latitude"], details["longitude"], details["loc"])
+    return render_template('pages/tools/ip_geolocation/details.html', details=details, map=map)
 
 @route.route('/number-geolocation')
 def number_geolocation():
---
+++ services/ip_geolocation.py
@@ -0,0 +1,21 @@
+import ipinfo
+import folium
+from dotenv import get_key
+
+class IpGeolocation:
+
+    def get_details(self, ip_address):
+        """Get IP geolocation."""
+        try:
+            access_token = get_key('.env', 'IPINFO_TOKEN')
+            handler = ipinfo.getHandler(access_token)
+            details = handler.getDetails(ip_address)
+            return details.all
+        except Exception as e:
+            return False
+
+    def draw_map(self, latitude, longitude, location):
+        """Draw map."""
+        map = folium.Map(location=[latitude, longitude], zoom_start=10)
+        folium.Marker([latitude, longitude], popup=location).add_to(map)
+        return map
