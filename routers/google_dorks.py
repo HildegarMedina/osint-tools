e8dd9c0ed6bb2049c26902fb6a4fc63085fec988
Hildegar Medina <medinahildegar1@gmail.com>
2024-11-23 19:02:22 -0300

    Refactor Google Dork for just generate

--- routers/google_dorks.py
+++ routers/google_dorks.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template, request, redirect, flash, jsonify
+from dotenv import get_key
 from modules.chatgpt import ChatGPT
 from services.google_dork import GoogleDork
-from dotenv import get_key
+from services.history import History
+from database import db
 
 router = Blueprint('google_dorks', __name__)
 
 chatgpt = ChatGPT()
-google_dork = GoogleDork(chatgpt)
+google_dork_svc = GoogleDork(chatgpt)
+history_svc = History(db)
 
 @router.route('/google-dorks', methods=['GET'])
 def index():
@@ -20,7 +23,8 @@ def generate():
     description = request.json.get('description')
     if not description:
         return jsonify({'error': 'Description is required'}), 400
-    dork = google_dork.generate(description)
+    dork = google_dork_svc.generate(description)
+    history_svc.save({"tool": "google-dorks", "input": description})
     return jsonify({
         "dork": dork.replace("Google Dork: ", "")
     })
