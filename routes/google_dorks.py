5fb91caa2e55b1286a8890dbdb19ce762145601e
Hildegar Medina <medinahildegar1@gmail.com>
2024-11-23 17:54:51 -0300

    Add page Google Dorks Generate and Search

--- routes/google_dorks.py
+++ routes/google_dorks.py
@@ -4,4 +4,4 @@ route = Blueprint('google_dorks', __name__)
 
 @route.route('/google-dorks', methods=['GET'])
 def index():
-    return render_template('pages/ip_geolocation/index.html')
+    return render_template('pages/google_dorks/index.html')
